7f00a865ee7d12342aad6d7508a239f73525ebd3
Cristian Aguilar <cristian.aguilar02@gmail.com>
2024-03-15 04:43:45 -0600

    variables en html

--- src/FirtApp.jsx
+++ src/FirtApp.jsx
@@ -1,10 +1,18 @@
+const persona = {
+  nombre : 'Cristian aguilar',
+  edad : 25,
+  soltero : true
+}
+
+const getSaludo = () => 'hola como estas';
 
 export const FirtApp = () => {
   return (
     // regla en React siempre agrupar en un elemento padre cuando se requiere retornar mas de dos elementos
     <>
-      <h1>Cristian Aguilar</h1>
-      <p>Hola mundo</p>
+      <h1>Informacion personal</h1>
+      <p>{JSON.stringify(persona) }</p>
+      <p>{getSaludo()}</p>
     </>
     
   )
