e8d796cfd1afc4147ad25d15b6a414f60f1fa9b7
Cristian Aguilar <cristian.aguilar02@gmail.com>
2024-04-09 04:11:12 -0600

    Eventos en react

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -3,10 +3,20 @@ import PropTypes from 'prop-types'
 // snipe para crear la estructura del componente rafc
 
 export const CounterApp = ({value}) => {
+
+  // funcion
+  const handleAdd = (event) => {
+    console.log(event);
+    console.log('haciendo clic +1');
+  }
   return (
     <>
         <h1>CounterApp</h1>
         <p>{value}</p>
+
+        <button onClick={ handleAdd }>
+          +1
+        </button>
     </>
   )
 }
